0633f0b37d6bd62be71ad23f2bdc84623c182d0c
chanlee20 <70008295+chanlee20@users.noreply.github.com>
2023-02-05 15:36:43 -0600

    xcode

---
+++ reLoved/reLoved/reLovedApp.swift
@@ -0,0 +1,17 @@
+//
+//  reLovedApp.swift
+//  reLoved
+//
+//  Created by 이찬 on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct reLovedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
