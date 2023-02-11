f3cde87bbdc08f26e1b7042d959f098659c183a8
Jiwoo Seo <jiwooseo@wustl.edu>
2023-02-10 18:08:19 -0600

    tabs added
    more works to be done

---
+++ reLoved/reLoved/chatView.swift
@@ -0,0 +1,20 @@
+//
+//  chatView.swift
+//  reLoved
+//
+//  Created by Jiwoo Seo on 2/10/23.
+//
+
+import SwiftUI
+
+struct chatView: View {
+    var body: some View {
+        Text("ChatView")
+    }
+}
+
+struct chatView_Previews: PreviewProvider {
+    static var previews: some View {
+        chatView()
+    }
+}
---
+++ reLoved/reLoved/profileView.swift
@@ -0,0 +1,20 @@
+//
+//  profileView.swift
+//  reLoved
+//
+//  Created by Jiwoo Seo on 2/10/23.
+//
+
+import SwiftUI
+
+struct profileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+struct profileView_Previews: PreviewProvider {
+    static var previews: some View {
+        profileView()
+    }
+}
